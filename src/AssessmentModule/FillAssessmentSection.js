99b2e26dcc2ac0312d888bb5142435c09ca99a99
wishtree-hsuthar <het.suthar@wishtreetech.com>
2022-10-17 16:10:34 +0530

    Fill table assessment | State management + Error handling

--- src/AssessmentModule/FillAssessmentSection.js
+++ src/AssessmentModule/FillAssessmentSection.js
@@ -1,19 +1,59 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import PreviewQuestions from "../Pages/PreviewQuestions";
+import FillAssessmentQuestion2 from "./FillAssessmentQuestion2";
 import FillAssessmentQuestion from "./FillAssessmentQuestions";
+
 function FillAssesmentSection({
-    // questionnaire,
-    // setQuestionnaire,
-    // uuid,
-    // setValue,
-    // index,
     section,
     setAssessmentQuestionnaire,
     assessmentQuestionnaire,
 }) {
     const navigate = useNavigate();
     const params = useParams();
+
+    const [answers, setAnswers] = useState({
+        [section?.uuid]: {},
+    });
+
+    const [errors, setErrors] = useState({
+        [section.uuid]: {},
+    });
+
+    const handleAnswersChange = (name, value) => {
+        let tempAns = answers[section?.uuid];
+        tempAns = { ...tempAns, [name]: value };
+        setAnswers({ [section?.uuid]: { ...tempAns } });
+    };
+
+    // useEffect(() => {
+    //     console.log("ANSWERS", answers);
+    // }, [answers]);
+
+    const handleFormSubmit = (e) => {
+        let tempErrors = errors[section?.uuid];
+        let currentSectionAnswers = answers[section?.uuid];
+
+        section?.rowValues.map((row) => {
+            row?.cells.map((cell) => {
+                if (
+                    !currentSectionAnswers[`${cell?.columnId}.${row?.uuid}`] ||
+                    currentSectionAnswers[`${cell?.columnId}.${row?.uuid}`]
+                        .length === 0
+                ) {
+                    tempErrors[`${cell?.columnId}.${row?.uuid}`] =
+                        "This is required";
+                } else {
+                    tempErrors[`${cell?.columnId}.${row?.uuid}`] = "";
+                }
+            });
+        });
+
+        setErrors({
+            [section?.uuid]: {
+                ...tempErrors,
+            },
+        });
+    };
 
     return (
         <>
@@ -29,16 +69,29 @@ function FillAssesmentSection({
             </div>
             <div className="preview-card-wrapper">
                 <div className="preview-que-wrap">
-                    {section.questions.map((question) => (
-                        <FillAssessmentQuestion
-                            assessmentQuestionnaire={assessmentQuestionnaire}
-                            setAssessmentQuestionnaire={
-                                setAssessmentQuestionnaire
-                            }
+                    {section.layout === "form" ? (
+                        section.questions.map((question) => (
+                            <FillAssessmentQuestion
+                                assessmentQuestionnaire={
+                                    assessmentQuestionnaire
+                                }
+                                setAssessmentQuestionnaire={
+                                    setAssessmentQuestionnaire
+                                }
+                                sectionUUID={section?.uuid}
+                                question={question}
+                            />
+                        ))
+                    ) : (
+                        <FillAssessmentQuestion2
                             sectionUUID={section?.uuid}
-                            question={question}
+                            columnValues={section?.columnValues}
+                            rowValues={section?.rowValues}
+                            answers={answers}
+                            handleAnswersChange={handleAnswersChange}
+                            errors={errors[section?.uuid]}
                         />
-                    ))}
+                    )}
                 </div>
                 <div className="form-btn flex-between add-members-btn mt-30">
                     <button
@@ -54,10 +107,12 @@ function FillAssesmentSection({
                     </button>
                     <button
                         type="submit"
-                        onClick={() =>
-                            navigate(
-                                `/questionnaires/add-questionnaire/${params.id}`
-                            )
+                        onClick={
+                            // () =>
+                            // navigate(
+                            //     `/questionnaires/add-questionnaire/${params.id}`
+                            // )
+                            handleFormSubmit
                         }
                         className="primary-button add-button"
                     >
