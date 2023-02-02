010a3b2da75b5d224857005330f3ab76e273b847
Rajkumar Patel <rajkumar.patel@wishtreetech.com>
2023-02-02 15:55:56 +0530

    - [x] Recall the APIs on Save and Add More (Add Member) | Parent company listing is not updated because of no latest call to fetch the parent company - [x] CGF Admin list - Searching with enter press

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -39,7 +39,7 @@ function Dropdown({
   const width = window.innerWidth;
   const charLimit = width > 1000 ? 60 : 45;
 
-  console.log("Device width:- ", width);
+  // console.log("Device width:- ", width);
   const Placeholder = ({ children }) => {
     return <div className="select-placeholder">{children}</div>;
   };
