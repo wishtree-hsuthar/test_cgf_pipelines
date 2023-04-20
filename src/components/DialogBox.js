d5e91737d580700a911e530210558d7876058283
wishtree-mjaveri <madhav.javeri@wishtreetech.com>
2023-04-20 11:13:54 +0530

    Add New Change : - [x] Add CSP in frontend index html file - [x] Show dailog box before deleting a member company if its operation members are active and download file containing this operation members - [x] Add change in dailog box to show button conditionally

--- src/components/DialogBox.js
+++ src/components/DialogBox.js
@@ -4,149 +4,137 @@ import PropTypes from "prop-types";
 import React from "react";
 
 const DialogBox = ({
-    title,
-    info1,
-    info2,
-    primaryButtonText,
-    secondaryButtonText,
-    onPrimaryModalButtonClickHandler,
-    isDisabledPrimaryButton = false,
-    onSecondaryModalButtonClickHandler,
-    openModal,
-    setOpenModal,
-    isModalForm,
-    handleCloseRedirect,
+  title,
+  info1,
+  info2,
+  primaryButtonText,
+  secondaryButtonText = undefined,
+  onPrimaryModalButtonClickHandler,
+  isDisabledPrimaryButton = false,
+  onSecondaryModalButtonClickHandler,
+  openModal,
+  setOpenModal,
+  isModalForm,
+  handleCloseRedirect,
 }) => {
-    // css for modal
-    const style = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        width: 400,
-        bgcolor: "background.paper",
-        border: "2px solid #000",
-        boxShadow: 24,
-        p: 4,
-    };
+  // css for modal
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
-    //modal button close
-    const handleClose = (event, reason) => {
-        if (reason && reason === "backdropClick") {
-            return;
-        }
-        setOpenModal(false);
-    };
+  //modal button close
+  const handleClose = (event, reason) => {
+    if (reason && reason === "backdropClick") {
+      return;
+    }
+    setOpenModal(false);
+  };
 
-    //on Primary modal button click handler to call function written in tester component
-    const onPrimaryButtonClickHandler = () => {
-        onPrimaryModalButtonClickHandler();
-    };
+  //on Primary modal button click handler to call function written in tester component
+  const onPrimaryButtonClickHandler = () => {
+    onPrimaryModalButtonClickHandler();
+  };
 
-    // on Secondary modal button click handler to call function written in tester component
-    const onSecondaryButtonClickHandler = () => {
-        onSecondaryModalButtonClickHandler();
-    };
-    let dialogTitle = "";
-    typeof title?.props?.children === "object"
-        ? title?.props?.children?.map((value) => (dialogTitle += value))
-        : (dialogTitle = title?.props?.children);
-    return (
-        <Modal
-            aria-labelledby="transition-modal-title"
-            aria-describedby="transition-modal-description"
-            open={openModal}
-            onClose={handleClose}
-            closeAfterTransition
-            disableEscapeKeyDown={isModalForm ? true : false}
-            BackdropComponent={Backdrop}
-            BackdropProps={{
-                timeout: 500,
-            }}
-            className="popup-blk"
-        >
-            <Fade in={openModal}>
-                <Box sx={style} className="popup-box">
-                    <div className="popup-innerblk">
-                        <div
-                            id="transition-modal-title"
-                            className="popup-ttl-blk"
-                        >
-                            {dialogTitle?.length <= 47 ? (
-                                <h2 className="popup-ttl heading2">{title}</h2>
-                            ) : (
-                                <Tooltip title={dialogTitle}>
-                                    <h2 className="popup-ttl heading2">
-                                        {" "}
-                                        {dialogTitle?.slice(0, 50)}..."
-                                    </h2>
-                                </Tooltip>
-                            )}
+  // on Secondary modal button click handler to call function written in tester component
+  const onSecondaryButtonClickHandler = () => {
+    onSecondaryModalButtonClickHandler();
+  };
+  let dialogTitle = "";
+  typeof title?.props?.children === "object"
+    ? title?.props?.children?.map((value) => (dialogTitle += value))
+    : (dialogTitle = title?.props?.children);
+  return (
+    <Modal
+      aria-labelledby="transition-modal-title"
+      aria-describedby="transition-modal-description"
+      open={openModal}
+      onClose={handleClose}
+      closeAfterTransition
+      disableEscapeKeyDown={isModalForm ? true : false}
+      BackdropComponent={Backdrop}
+      BackdropProps={{
+        timeout: 500,
+      }}
+      className="popup-blk"
+    >
+      <Fade in={openModal}>
+        <Box sx={style} className="popup-box">
+          <div className="popup-innerblk">
+            <div id="transition-modal-title" className="popup-ttl-blk">
+              {dialogTitle?.length <= 47 ? (
+                <h2 className="popup-ttl heading2">{title}</h2>
+              ) : (
+                <Tooltip title={dialogTitle}>
+                  <h2 className="popup-ttl heading2">
+                    {" "}
+                    {dialogTitle?.slice(0, 50)}..."
+                  </h2>
+                </Tooltip>
+              )}
 
-                            <span
-                                // hidden={isModalForm}
-                                className="popup-close-icon"
-                                onClick={
-                                    isModalForm
-                                        ? handleCloseRedirect
-                                        : handleClose
-                                }
-                            >
-                                <CloseIcon />
-                            </span>
-                        </div>
-                        <div
-                            id="transition-modal-description"
-                            className="popup-body"
-                        >
-                            <div className="popup-content-blk text-center">
-                                {info1}
-                                {isModalForm ? (
-                                    <form
-                                        onSubmit={onSecondaryButtonClickHandler}
-                                    >
-                                        {info2}
-                                    </form>
-                                ) : (
-                                    info2
-                                )}
-                                <div className="form-btn flex-center">
-                                    <button
-                                        type="submit"
-                                        onClick={onSecondaryButtonClickHandler}
-                                        className="secondary-button mr-10"
-                                    >
-                                        {secondaryButtonText}
-                                    </button>
-                                    <button
-                                        type="submit"
-                                        onClick={onPrimaryButtonClickHandler}
-                                        disabled={isDisabledPrimaryButton}
-                                        className="primary-button"
-                                    >
-                                        {primaryButtonText}
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Box>
-            </Fade>
-        </Modal>
-    );
+              <span
+                // hidden={isModalForm}
+                className="popup-close-icon"
+                onClick={isModalForm ? handleCloseRedirect : handleClose}
+              >
+                <CloseIcon />
+              </span>
+            </div>
+            <div id="transition-modal-description" className="popup-body">
+              <div className="popup-content-blk text-center">
+                {info1}
+                {isModalForm ? (
+                  <form onSubmit={onSecondaryButtonClickHandler}>{info2}</form>
+                ) : (
+                  info2
+                )}
+                <div className="form-btn flex-center">
+                  {secondaryButtonText && (
+                    <button
+                      type="submit"
+                      onClick={onSecondaryButtonClickHandler}
+                      className="secondary-button mr-10"
+                    >
+                      {secondaryButtonText}
+                    </button>
+                  )}
+                  <button
+                    type="submit"
+                    onClick={onPrimaryButtonClickHandler}
+                    disabled={isDisabledPrimaryButton}
+                    className="primary-button"
+                  >
+                    {primaryButtonText}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </Box>
+      </Fade>
+    </Modal>
+  );
 };
 
 export default DialogBox;
 
 DialogBox.propTypes = {
-    title: PropTypes.object,
-    info1: PropTypes.object,
-    info2: PropTypes.object,
-    primaryButtonText: PropTypes.string,
-    secondaryButtonText: PropTypes.string,
-    onPrimaryModalButtonClickHandler: PropTypes.func,
-    onSecondaryModalButtonClickHandler: PropTypes.func,
-    openModal: PropTypes.bool,
-    setOpenModal: PropTypes.func,
-    isModalForm: PropTypes.bool,
+  title: PropTypes.object,
+  info1: PropTypes.object,
+  info2: PropTypes.object,
+  primaryButtonText: PropTypes.string,
+  secondaryButtonText: PropTypes.string,
+  onPrimaryModalButtonClickHandler: PropTypes.func,
+  onSecondaryModalButtonClickHandler: PropTypes.func,
+  openModal: PropTypes.bool,
+  setOpenModal: PropTypes.func,
+  isModalForm: PropTypes.bool,
 };
