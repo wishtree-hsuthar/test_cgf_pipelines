e0da4c4128f28ff5c0162da8d449e597e1cadcf6
wishtree-mjaveri <madhav.javeri@wishtreetech.com>
2023-05-24 17:22:33 +0530

    Tried optimizing code using lazy loading in app js file, slider js for lcp images

--- src/App.js
+++ src/App.js
@@ -5,45 +5,48 @@ import "./responsive.css";
 import axios from "axios";
 import { Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
-import FallBackUI from "./Pages/FallBackUI";
-import ForgetPassword from "./Pages/ForgetPassword";
-import Login from "./Pages/Login";
-import ResetPassword from "./Pages/ResetPassword";
-import SetPassword from "./Pages/SetPassword";
-import ProtectedPages from "./router/ProtectedPages";
+// import FallBackUI from "./Pages/FallBackUI";
+// import ForgetPassword from "./Pages/ForgetPassword";
+// import Login from "./Pages/Login";
+// import ResetPassword from "./Pages/ResetPassword";
+// import SetPassword from "./Pages/SetPassword";
+// import ProtectedPages from "./router/ProtectedPages";
 import UserLoggedIn from "./router/UserLoggedIn";
 import Loader from "./utils/Loader";
+const Login = React.lazy(() => import("./Pages/Login"));
+const ProtectedPages = React.lazy(() => import("./router/ProtectedPages"));
+const SetPassword = React.lazy(() => import("./Pages/SetPassword"));
+const ResetPassword = React.lazy(() => import("./Pages/ResetPassword"));
+const FallBackUI = React.lazy(() => import("./Pages/FallBackUI"));
+const ForgetPassword = React.lazy(() => import("./Pages/ForgetPassword"));
 axios.defaults.withCredentials = true;
 function App() {
-    return (
-        <React.Fragment>
-            <Suspense fallback={<Loader />}>
-                <Routes>
-                    {/* <Route path="/questionnare" element={<AddQuestionnaires/>}/> */}
-                    <Route path="/login" element={<Login />} />
-                    <Route
-                        path="/forget-password"
-                        element={<ForgetPassword />}
-                    />
-                    {/* <Route path="auth/*" element={<AuthOutlet />} /> */}
-                    <Route path="auth/confirm/:id" element={<SetPassword />} />
-                    <Route path="auth/forgot/:id" element={<ResetPassword />} />
-                    {/* <Route path="/*" element={<Layout />}> */}
-                    <Route
-                        path={"/*"}
-                        element={
-                            <UserLoggedIn>
-                                <ProtectedPages />
-                            </UserLoggedIn>
-                        }
-                    />
-                    {/* </Route> */}
-                    <Route element={<FallBackUI />} />
-                </Routes>
-            </Suspense>
-            <Footer />
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          {/* <Route path="/questionnare" element={<AddQuestionnaires/>}/> */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/forget-password" element={<ForgetPassword />} />
+          {/* <Route path="auth/*" element={<AuthOutlet />} /> */}
+          <Route path="auth/confirm/:id" element={<SetPassword />} />
+          <Route path="auth/forgot/:id" element={<ResetPassword />} />
+          {/* <Route path="/*" element={<Layout />}> */}
+          <Route
+            path={"/*"}
+            element={
+              <UserLoggedIn>
+                <ProtectedPages />
+              </UserLoggedIn>
+            }
+          />
+          {/* </Route> */}
+          <Route element={<FallBackUI />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
