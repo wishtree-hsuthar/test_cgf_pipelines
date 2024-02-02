7c8e0e985869bd0ddad0a92138b54f7c0d66c65b
wishtree-mjaveri <madhav.javeri@wishtreetech.com>
2024-02-02 14:47:00 +0530

    CSS styling - [x] Dashboard filter component - [x] Assessment listing - [x] Company SAQ/Country SAQ stat table

--- src/Pages/Dashboard/SAQStatsTable.js
+++ src/Pages/Dashboard/SAQStatsTable.js
@@ -9,14 +9,14 @@ import Paper from '@mui/material/Paper';
 function SAQStatsTable({records,type}) {
   return (
     <div>
-           <TableContainer component={Paper}>
+           <TableContainer component={Paper} className='table-blk'>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>{type}</TableCell>
-            <TableCell align="right">Submitted</TableCell>
-            <TableCell align="right">Pending</TableCell>
-            <TableCell align="right">Total</TableCell>
+            <TableCell >Submitted</TableCell>
+            <TableCell>Pending</TableCell>
+            <TableCell>Total</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -28,9 +28,9 @@ function SAQStatsTable({records,type}) {
               <TableCell component="th" scope="row">
                 {type==='Member Company'?row?.memberCompany:row.country}
               </TableCell>
-              <TableCell align="right">{row?.submitted}</TableCell>
-              <TableCell align="right">{row?.pending}</TableCell>
-              <TableCell align="right">{row?.total}</TableCell>
+              <TableCell >{row?.submitted}</TableCell>
+              <TableCell >{row?.pending}</TableCell>
+              <TableCell >{row?.total}</TableCell>
             </TableRow>
           ))}
         </TableBody>
